79d9cb2efd3b81d5ca4174b65ae396305b171efe
Robin Mattheussen <robin.mattheussen@gmail.com>
2020-10-26 15:28:29 +0100

    First example of a handler

--- mattermost-http/src/api/user.rs
+++ mattermost-http/src/api/user.rs
@@ -1,9 +1,59 @@
-use actix_web::{web, Responder};
+use actix_web::{web, Responder, HttpResponse};
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
-pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/users").route("", web::get().to(hello_world)));
+#[derive(Deserialize)]
+struct CreateUserQuery {
+    t: String,
+    iid: String,
 }
 
-async fn hello_world() -> impl Responder {
-    "Hello World!"
+#[derive(Deserialize, Serialize)]
+struct CreateUser {
+    email: String,
+    username: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    nickname: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    first_name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    last_name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    auth_data: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    auth_service: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    password: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    locale: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    props: Option<HashMap<String, serde_json::Value>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    notify_props: Option<NotifyProps>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct NotifyProps {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    email: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    push: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    desktop: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    desktop_sound: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    mention_keys: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    channel: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    first_name: Option<bool>,
+}
+
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/users").route("", web::post().to(create_user)));
+}
+
+async fn create_user(query: web::Query<CreateUserQuery>, item: web::Json<CreateUser>) -> impl Responder {
+    HttpResponse::Created().json(item.0)
 }
